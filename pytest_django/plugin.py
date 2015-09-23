cfab495d52496cd66bba5dc65e6280545e2f85ae
Jakub Owczarski <jakub3279@gmail.com>
2015-09-23 10:05:47 +0000

    Add ``django_use_model`` marker that allows testing of unmanaged models

--- pytest_django/plugin.py
+++ pytest_django/plugin.py
@@ -168,6 +168,10 @@ def pytest_load_initial_conftests(early_config, parser, args):
         'the `urls` attribute of Django `TestCase` objects.  *modstr* is '
         'a string specifying the module of a URL config, e.g. '
         '"my_app.test_urls".')
+    early_config.addinivalue_line(
+        'markers',
+        'django_use_model(model): force model creation, '
+        'even for unmanaged models. Model(s) are deleted at the end of scope')
 
     options = parser.parse_known_args(args)
 
@@ -295,6 +299,58 @@ def _django_db_marker(request):
             request.getfuncargvalue('transactional_db')
         else:
             request.getfuncargvalue('db')
+
+
+@pytest.fixture(autouse=True)
+def _django_use_model(request):
+    """Implement ``django_use_model`` marker.
+
+    Marker creates unmanaged models that normally aren't created.
+    Destroys it at the end of marked scope.
+
+    Note that you still need to use ``django_db`` marker before this one.
+    The test unit should be decorated:
+
+    @pytest.mark.django_db
+    @pytest.mark.django_use_model(model)
+
+    :model: ModelClass or 'app.ModelName', one or many
+    """
+    marker = request.keywords.get('django_use_model', None)
+    if not marker:
+        return
+    from django.db import connection
+    from django.db.utils import OperationalError
+
+    model = request.getfuncargvalue('model')
+
+    if isinstance(model, (list, tuple)):
+        models = model
+    else:
+        models = (model,)
+
+    def _model_class(inp):
+        if isinstance(inp, str):
+            from django.apps import apps
+            return apps.get_model(inp)
+        return inp
+
+    model_classes = [_model_class(x) for x in models]
+
+    with contextlib.closing(connection.schema_editor()) as schema:
+        schema.deferred_sql = []
+        for model_class in model_classes:
+            try:
+                schema.create_model(model_class)
+            except OperationalError:
+                pass
+
+    def drop():
+        with contextlib.closing(connection.schema_editor()) as schema:
+            for model_class in model_classes:
+                schema.delete_model(model_class)
+
+    request.addfinalizer(drop)
 
 
 @pytest.fixture(autouse=True, scope='class')
